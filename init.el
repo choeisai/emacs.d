ce505db1871f857893fa30a91dbbafc18cdd54ce
Chalermrat Srinara <schalermrat@gmail.com>
2017-05-22 00:26:00 +0700

    feat(garbage-collection): Add garbage collection setup

--- init.el
+++ init.el
@@ -4,3 +4,12 @@
 (require 'init-benchmarking) ;; Measure startup time
 
 (defconst *is-a-mac* (eq system-type 'darwin))
+
+;;----------------------------------------------------------------------------
+;; Temporarily reduce garbage collection during startup
+;;----------------------------------------------------------------------------
+(defconst sanityinc/initial-gc-cons-threshold gc-cons-threshold
+  "Initial value of `gc-cons-threshold' at start-up time.")
+(setq gc-cons-threshold (* 128 1024 1024))
+(add-hook 'after-init-hook
+          (lambda () (setq gc-cons-threshold sanityinc/initial-gc-cons-threshold)))
